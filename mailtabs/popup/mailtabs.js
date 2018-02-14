1554b74d1c64960eafd93e0c628a0ef965665656
spon000 <spon000@yahoo.com>
2018-02-14 17:30:00 -0500

    Add files via upload

--- mailtabs/popup/mailtabs.js
+++ mailtabs/popup/mailtabs.js
@@ -1,90 +1,58 @@
-// /**
- // * Listen for clicks on the buttons, and send the appropriate message to
- // * the content script in the page.
- // */
-// function listenForClicks() {
-  // document.addEventListener("click", (e) => {
+$(document).ready(function(){
+	var queryInfo = {};
+	var recipient = "";
+	var subject = "";
+	
+	getStoredOptions();
 
-    // /**
-     // * Given the name of a beast, get the URL to the corresponding image.
-     // */
-    // function beastNameToURL(beastName) {
-      // switch (beastName) {
-        // case "Frog":
-          // return browser.extension.getURL("beasts/frog.jpg");
-        // case "Snake":
-          // return browser.extension.getURL("beasts/snake.jpg");
-        // case "Turtle":
-          // return browser.extension.getURL("beasts/turtle.jpg");
-      // }
-    // }
+	$("#mailit-button").click((e) => {
+		queryInfo = getAllWindowsBox();
+		browser.tabs.query(queryInfo).then((result) => {
+			var mailToBody = encodeURI(createTabDoc(result));
+			openMailDialog(mailToBody);
+		});
+	});
+	
+	$("#saveit-button").click((e) => {
+		queryInfo = getAllWindowsBox();
+		
+	});
+	
+	function getAllWindowsBox() {
+		if (!($("#allWindowsCheck")[0].checked)) 
+			return {"currentWindow":true};
+		else 
+			return {};
+	}
+	
+	function getStoredOptions() {
+		
+	}
+	
+	function createTabDoc(tabs) {
+		var tabsContent = "\n";
+		tabs.forEach((tab) => {
+			tabsContent += tab.title + "\n";
+			tabsContent += tab.url + "\n";
+			tabsContent += "-".repeat(50) + "\n\n";
+		});
+		return tabsContent;
+	}
+	
+	function openMailDialog(tabDoc) {
+		if ($("#recipientText")[0].value) {
+			recipient = $("#recipientText")[0].value;
+		}
+		
+		if ($("#subjectText")[0].value) {
+			subject = $("#subjectText")[0].value;
+		}
+		document.location = "mailto:"+recipient+"?subject="+subject+"&body="+tabDoc;
+	}
+	
+	function saveToClipboard(tabDoc) {
+		
+	}
 
-    // /**
-     // * Insert the page-hiding CSS into the active tab,
-     // * then get the beast URL and
-     // * send a "beastify" message to the content script in the active tab.
-     // */
-    // function beastify(tabs) {
-      // browser.tabs.insertCSS({code: hidePage}).then(() => {
-        // let url = beastNameToURL(e.target.textContent);
-        // browser.tabs.sendMessage(tabs[0].id, {
-          // command: "beastify",
-          // beastURL: url
-        // });
-      // });
-    // }
+}); 
 
-    // /**
-     // * Remove the page-hiding CSS from the active tab,
-     // * send a "reset" message to the content script in the active tab.
-     // */
-    // function reset(tabs) {
-      // browser.tabs.removeCSS({code: hidePage}).then(() => {
-        // browser.tabs.sendMessage(tabs[0].id, {
-          // command: "reset",
-        // });
-      // });
-    // }
-
-    // /**
-     // * Just log the error to the console.
-     // */
-    // function reportError(error) {
-      // console.error(`Could not beastify: ${error}`);
-    // }
-
-    // /**
-     // * Get the active tab,
-     // * then call "beastify()" or "reset()" as appropriate.
-     // */
-    // if (e.target.classList.contains("beast")) {
-      // browser.tabs.query({active: true, currentWindow: true})
-        // .then(beastify)
-        // .catch(reportError);
-    // }
-    // else if (e.target.classList.contains("reset")) {
-      // browser.tabs.query({active: true, currentWindow: true})
-        // .then(reset)
-        // .catch(reportError);
-    // }
-  // });
-// }
-
-// /**
- // * There was an error executing the script.
- // * Display the popup's error message, and hide the normal UI.
- // */
-// function reportExecuteScriptError(error) {
-  // document.querySelector("#popup-content").classList.add("hidden");
-  // document.querySelector("#error-content").classList.remove("hidden");
-  // console.error(`Failed to execute beastify content script: ${error.message}`);
-// }
-
-// /**
- // * When the popup loads, inject a content script into the active tab,
- // * and add a click handler.
- // * If we couldn't inject the script, handle the error.
- // */
-// browser.tabs.executeScript({file: "/content_scripts/beastify.js"})
-// .then(listenForClicks)
-// .catch(reportExecuteScriptError);
